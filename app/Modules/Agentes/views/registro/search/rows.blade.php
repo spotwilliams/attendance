b11a5e5a3e0a110c17c8a6bf95d2f27d2a4d4b68
Richard V. 121 <ricardo@121ecommerce.com>
2017-06-26 22:33:35 -0300

    Fix

--- app/Modules/Agentes/views/registro/search/rows.blade.php
+++ app/Modules/Agentes/views/registro/search/rows.blade.php
@@ -2,7 +2,10 @@
     <tr>
         <td>{{$agente->apellido}}, {{$agente->nombre}}</td>
         <td>{{$agente->cuit}}</td>
-        <td>@if(isset($agente->operativo))$agente->operativo->base->nombre)@endif</td>
+        <td>@if(isset($agente->operativo))
+                {{$agente->operativo->base->nombre}}
+            @endif
+        </td>
         @include('Agentes::registro.commons.operaciones-celda')
     </tr>
-@endforeach
+    @endforeach
