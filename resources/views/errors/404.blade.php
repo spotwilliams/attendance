bc24ae278ca194521bbc7b3d504729ac1c112a71
Richard V. 121 <ricardo@121ecommerce.com>
2017-06-26 22:43:16 -0300

    Menu

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -10,7 +10,7 @@
                 <h2 class="headline text-yellow"><i class="fa fa-warning text-yellow"></i> No era por aqu&iacute;!</h2>
 
                 <p>
-                    Por favor, utilice el mem&uacute; para ir hacia la secci&oacute;n deseada
+                    Por favor, utilice el men&uacute; para ir hacia la secci&oacute;n deseada
                 </p>
 
             </div>
