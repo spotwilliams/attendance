b0a40f6d5bc6685eb7054fdb50905d8959d6cc7d
Richard V. 121 <ricardo@121ecommerce.com>
2017-07-11 10:27:58 -0300

    Fix regla dinamica para estado de contrato

--- app/Http/Helpers/Validation.php
+++ app/Http/Helpers/Validation.php
@@ -10,6 +10,7 @@ namespace Cat\Helpers;
 
 
 use Cat\Models\Contrato;
+use Cat\Models\EstadoContrato;
 use Cat\Models\TipoContrato;
 use Illuminate\Http\Request;
 
@@ -23,7 +24,7 @@ class Validation
             'domicilio.numero.0'      => 'required|integer',
         ];
         $domiciliosInput = $request->input('domicilio');
-
+        
         if (!empty($domiciliosInput['numero'][1])) {
             
             $rules['domicilio.calle.1']  = 'required';
@@ -36,6 +37,12 @@ class Validation
     public static function getContratoRules(Request $request)
     {
         $rules = Contrato::$rules;
+        
+        /**
+         *
+         * Por el tipo de contrato
+         *
+         */
         if ($request->input('id_tipo_contrato') == -1) {
             return $rules;
         }
@@ -43,8 +50,20 @@ class Validation
         $tipoContrato = TipoContrato::find($request->input('id_tipo_contrato'));
         
         if (!$tipoContrato->isLocacion()) {
-//            unset($rules['fecha_ingreso']);
             unset($rules['tipo_inscripcion']);
+        }
+        /**
+         *
+         * Por el estado de contrato
+         *
+         */
+        if ($request->input('id_estado_contrato') == -1) {
+            return $rules;
+        }
+        /** @var EstadoContrato $estadoContrato */
+        $estadoContrato = EstadoContrato::find($request->input('id_estado_contrato'));
+        if ($estadoContrato->esActivo()) {
+            unset($rules['fecha_baja']);
         }
         
         return $rules;
