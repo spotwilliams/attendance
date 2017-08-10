b40bbe38af40c219529f0cee9d1f8bb9b9feadfc
Richard V. 121 <ricardo@121ecommerce.com>
2017-08-10 16:15:59 -0300

    Presentismos - Manual - Filtros opcioales

--- app/Modules/Presentismo/Controllers/GeneralController.php
+++ app/Modules/Presentismo/Controllers/GeneralController.php
@@ -49,8 +49,8 @@ class GeneralController extends AppBaseController
     {
         $this->validate($request, [
             'base'   => 'required|not_in:-1',
-            'turnos' => 'required',
-            'areas'  => 'required',
+//            'turnos' => 'required',
+            //            'areas'  => 'required',
         ]);
         
         $input = $request->all();
@@ -58,8 +58,9 @@ class GeneralController extends AppBaseController
         return $this->listaAgentes(
             $request,
             $input['base'],
-            $input['turnos'],
-            $input['areas'],
+            (isset($input['turnos']) ? $input['turnos'] : []),
+            (isset($input['areas']) ? $input['areas'] : []),
+//            $input['areas'],
             $input['desde'],
             $input['hasta'],
             (isset($input['funcion']) ? $input['funcion'] : [])
@@ -97,12 +98,17 @@ class GeneralController extends AppBaseController
                     $base,
                     $dateRange['desde'],
                     $dateRange['hasta']
-                )
-                ->whereIn('operativos.id_turno', $turnos)
-                ->whereIn('operativos.id_area', $areas);
+                );
+            if (!empty($turnos)) {
+                $agentes->whereIn('operativos.id_turno', $turnos);
+            }
+
+            if (!empty($areas)) {
+                $agentes->whereIn('operativos.id_area', $areas);
+            }
             // En caso que pasemos una funcion la buscamos, sino la excluimos desde sql
             if (!empty($funcion)) {
-    
+                
                 $agentes->whereIn('operativos.id_funcion', $funcion);
             }
             $agentes->with('contrato.tipoContrato');
