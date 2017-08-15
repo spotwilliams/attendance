c642af6bf8294faa4659a9216157b7a3ecc9f293
Richard V. 121 <ricardo@121ecommerce.com>
2017-08-15 15:00:52 -0300

    Close Conf on tp

--- app/Modules/Configuracion/routes.php
+++ app/Modules/Configuracion/routes.php
@@ -8,6 +8,6 @@ Route::group(['prefix' => 'configuracion', 'middleware' => ['web']], function ()
     Route::resource('area', \Cat\Modules\Configuracion\Areas\Controllers\CrudController::class);
     Route::resource('turno', \Cat\Modules\Configuracion\Turnos\Controllers\CrudController::class);
     
-    Route::resource('licencia', \Cat\Modules\Configuracion\TipoPresentismos\Controllers\CrudController::class);
+//    Route::resource('licencia', \Cat\Modules\Configuracion\TipoPresentismos\Controllers\CrudController::class);
 
 });
