7f203520e94801f20a104206d0b8b5b96455b24f
Richard V. 121 <ricardo@121ecommerce.com>
2017-09-24 22:06:09 -0300

    Comentarios en reporte

---
+++ app/Modules/Reportes/views/common/comentarios.blade.php
@@ -0,0 +1,19 @@
+<span class="label label-default"
+      data-toggle="popover"
+      title="Comentarios"
+      data-placement="bottom"
+      data-content="<div>
+@foreach($comentarios as $comment)
+              <p class='text-muted well well-sm no-shadow'>
+                {{$comment->comentario}}
+                (<span class='label label-info'>{{$comment->user->email}}</span>)
+                (<span class='label label-info'>{{(new DateTime($comment->created_at))->format('d/m/Y')}}</span>
+              </p>
+
+      @endforeach
+              </div>
+              "
+      data-html="true"
+>
+                            <i class="fa fa-comment-o"></i>
+    </span>
