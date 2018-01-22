50c898631796e0616470ca1a46dfc55cab634716
Richard V. 121 <ricardo@121ecommerce.com>
2018-01-22 14:55:37 -0300

    Se eliminaron las migraciones

--- app/Modules/Masivo/views/inicial/advertisment.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="box box-warning instrucciones hidden ">
-    <div class="box-header">
-        Instrucciones
-    </div>
-    <div class="box-body">
-        <a href="{{route('agentesMasivoTemplate')}}">Ver archivo ejemplo</a>
-    </div>
-</div>
-
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -39,18 +39,6 @@
                 <span>Alta masiva</span>
             </a>
         </li>
-        {{--<li>--}}
-            {{--<a href="{!! route('agentesMasivoUpdateIndex') !!}">--}}
-                {{--<i class="fa fa-file-excel-o"></i>--}}
-                {{--<span>Update masiva</span>--}}
-            {{--</a>--}}
-        {{--</li>--}}
-        {{--<li>--}}
-            {{--<a href="{!! route('agentesMasivoDuplicadoIndex') !!}">--}}
-                {{--<i class="fa fa-file-excel-o"></i>--}}
-                {{--<span>Duplicado masiva</span>--}}
-            {{--</a>--}}
-        {{--</li>--}}
     </ul>
 </li>
 
@@ -84,18 +72,6 @@
                 <span>Registro por agente</span>
             </a>
         </li>
-        <li>
-            <a href="{!! route('presentismosMasivoMigracionIndex') !!}">
-                <i class="fa fa-truck"></i>
-                <span>Migraci&oacute;n</span>
-            </a>
-        </li>
-        {{--<li>--}}
-        {{--<a href="{!! route('presentismosInicialMasivoIndex') !!}">--}}
-        {{--<i class="fa fa-desktop"></i>--}}
-        {{--<span>Incial</span>--}}
-        {{--</a>--}}
-        {{--</li>--}}
     </ul>
 </li>
 
