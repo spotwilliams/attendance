0b8546e4ceadd73761b531fe89c8db06c504d70e
Richard V. 121 <ricardo@121ecommerce.com>
2018-03-21 15:18:56 -0300

    Seeder

--- database/migrations/2018_03_21_122930_create_contratos_historicos_table.php
+++ database/migrations/2018_03_21_122930_create_contratos_historicos_table.php
@@ -55,6 +55,26 @@ class CreateContratosHistoricosTable extends Migration
                 ->onDelete('NO ACTION');
             
         });
+        
+        \Illuminate\Support\Facades\DB::insert(
+            'INSERT INTO contratos_historicos (id_tipo_contrato, id_estado_contrato, id_agente, fecha_ingreso, fecha_ingreso_gobierno, fecha_fin, fecha_estado_desde, fecha_estado_hasta, id_sial, ficha, tipo_inscripcion, comentario, monto)
+              SELECT
+                id_tipo_contrato,
+                id_estado_contrato,
+                id_agente,
+                fecha_ingreso,
+                fecha_ingreso_gobierno,
+                fecha_fin,
+                fecha_estado_desde,
+                fecha_estado_hasta,
+                id_sial,
+                ficha,
+                tipo_inscripcion,
+                comentario,
+                monto
+              FROM contratos_historicos;
+');
+    
     }
     
     /**
