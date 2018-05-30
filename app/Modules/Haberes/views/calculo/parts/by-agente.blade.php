1d077bfee275212fb426474e7dc59df40149b833
Richard V. 121 <ricardo@121ecommerce.com>
2018-05-30 11:17:35 -0300

    Improve view by-agente

--- app/Modules/Haberes/views/calculo/parts/by-agente.blade.php
+++ app/Modules/Haberes/views/calculo/parts/by-agente.blade.php
@@ -1,18 +1,17 @@
 {{ Form::open(['route' => 'haberesSearchByAgente', 'method' => 'POST'])}}
+
 <input type="hidden" name="periodo" value="{{$periodo->id}}">
-<div class="col-md-4">
+<div class="col-md-4 form-group">
     {{ Form::text('apellido', Request::input('apellido'), ['id' => 'apellido', 'placeholder' => 'Apellido', 'class' => 'col-md-3 form-control']) }}
 </div>
-<div class="col-md-4">
+<div class="col-md-4 form-group">
     {{ Form::text('nombre', Request::input('nombre'), ['id' => 'nombre', 'placeholder' => 'Nombre', 'class' => 'col-md-3 form-control']) }}
 </div>
-<div class="col-md-4">
-    <div class="input-group">
-        {{ Form::text('cuit', Request::input('cuit'), ['id' => 'cuit', 'placeholder' => 'CUIT', 'class' => 'form-control']) }}
-        <span class="input-group-btn">
-                                {{ Form::submit('Buscar', ['class' => 'btn btn-info btn-flat']) }}
-                            </span>
-    </div>
+<div class="col-md-4 form-group">
+    {{ Form::text('cuit', Request::input('cuit'), ['id' => 'cuit', 'placeholder' => 'CUIT', 'class' => 'form-control']) }}
+</div>
+<div class="col-md-12 form-group">
+    {{ Form::submit('Buscar', ['class' => 'btn btn-info btn-flat pull-right']) }}
 </div>
 {{ Form::close() }}
 @section('scripts')
