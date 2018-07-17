900af747d62ce80084bcd69621e0ba9c81bb4716
Richard V. 121 <ricardo@121ecommerce.com>
2018-07-17 14:17:28 -0300

    Creacion de migracion para agregar el track de la cant de agentes

---
+++ database/migrations/2018_07_17_131250_add_track_cant_agentes.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTrackCantAgentes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        
+        \Cat\Models\Param::create([
+        
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
