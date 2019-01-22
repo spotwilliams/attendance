97957c621ec575654ebb1a8a21802b27e477b059
Ricardo Vergara <ricardovergarawork@gmail.com>
2019-01-22 13:06:58 -0300

    Trim en los inputs con un middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends HttpKernel
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Session\Middleware\StartSession::class,
+        \Cat\Http\Middleware\InputTrim::class
     ];
 
     /**
---
+++ app/Http/Middleware/InputTrim.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Cat\Http\Middleware;
+
+use Closure;
+
+class InputTrim
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // credit to @t202wes - https://gist.github.com/drakakisgeo/3bba2a2600b4c554f836#gistcomment-1970006
+        $input = $request->all();
+        if ($input) {
+            array_walk_recursive($input, function (&$item) {
+                $item = trim($item);
+                $item = ($item == "") ? null : $item;
+            });
+            $request->merge($input);
+        }
+        return $next($request);
+    }
+}
