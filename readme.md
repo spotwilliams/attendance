679c1bfb9d7c8bd37aebce9d316a3dd3b025b641
Ricardo Vergara <ricardovergarawork@gmail.com>
2019-09-09 10:04:10 -0300

    First commit: Including
    * Debuger bar
    * Voyager
    * Telescope

---
+++ readme.md
@@ -0,0 +1,10 @@
+
+## About this project
+
+Simple project to register the attendance to sanitary event. 
+
+
+## Dependencies
+
+* Laravel 5.8
+* Voyager
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+@extends('voyager::master')
+
+@section('page_title', 'Home')
+
+@section('content')
+    <div class="page-content browse container-fluid">
+        @include('voyager::alerts')
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-bordered">
+                    <div class="panel-body">
+                        <div class="table-responsive">
+                            <table id="dataTable" class="table table-hover">
+                                <thead>
+                                </thead>
+                                <tbody>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
