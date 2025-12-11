d4a42978d360fd148bae537181c5043506461402
Ricardo Vergara <richard@redcatsystems.com>
2025-12-11 12:36:23 +0300

    upgrade/laravel-6.0 routes updates to allow display views

--- app/Modules/Configuracion/routes.php
+++ app/Modules/Configuracion/routes.php
@@ -2,26 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'configuracion', 'middleware' => ['web']], function (): void {
-    
-    Route::resource('base', \Cat\Modules\Configuracion\Bases\Controllers\CrudController::class);
-    Route::resource('area', \Cat\Modules\Configuracion\Areas\Controllers\CrudController::class);
-    Route::resource('turno', \Cat\Modules\Configuracion\Turnos\Controllers\CrudController::class);
-    Route::resource('licencia', \Cat\Modules\Configuracion\TipoPresentismos\Controllers\CrudController::class);
+Route::name('configuracion.')
+    ->prefix('configuracion')
+    ->middleware('web')
+    ->group(function (): void {
 
-    Route::get('fecha/cierre', \Cat\Modules\Configuracion\FechaCierrePeriodo\Controllers\CrudController::class . '@index')
-        ->name('configuracion.fecha.cierre.index');
+        Route::resource('base', \Cat\Modules\Configuracion\Bases\Controllers\CrudController::class);
+        Route::resource('area', \Cat\Modules\Configuracion\Areas\Controllers\CrudController::class);
+        Route::resource('turno', \Cat\Modules\Configuracion\Turnos\Controllers\CrudController::class);
+        Route::resource('licencia', \Cat\Modules\Configuracion\TipoPresentismos\Controllers\CrudController::class);
 
-    Route::post('fecha/cierre', \Cat\Modules\Configuracion\FechaCierrePeriodo\Controllers\CrudController::class . '@update')
-        ->name('configuracion.fecha.cierre.update');
-    
-    /**
-     * Deletes
-     */
-    
-    Route::get('base/delete/id/{id}', \Cat\Modules\Configuracion\Bases\Controllers\CrudController::class.'@delete')->name('configuracion.base.delete');
-    Route::get('area/delete/id/{id}', \Cat\Modules\Configuracion\Areas\Controllers\CrudController::class.'@delete')->name('configuracion.area.delete');
-    Route::get('turno/delete/id/{id}', \Cat\Modules\Configuracion\Turnos\Controllers\CrudController::class.'@delete')->name('configuracion.turno.delete');
-    Route::get('licencia/delete/id/{id}', \Cat\Modules\Configuracion\TipoPresentismos\Controllers\CrudController::class.'@delete')->name('configuracion.licencia.delete');
-    
-});
+    });
+
+// Routes with custom names
+Route::middleware('web')
+    ->group(function () {
+
+        /**
+         * Deletes
+         */
+
+        Route::get('base/delete/id/{id}', \Cat\Modules\Configuracion\Bases\Controllers\CrudController::class . '@delete')->name('configuracion.base.delete');
+        Route::get('area/delete/id/{id}', \Cat\Modules\Configuracion\Areas\Controllers\CrudController::class . '@delete')->name('configuracion.area.delete');
+        Route::get('turno/delete/id/{id}', \Cat\Modules\Configuracion\Turnos\Controllers\CrudController::class . '@delete')->name('configuracion.turno.delete');
+        Route::get('licencia/delete/id/{id}', \Cat\Modules\Configuracion\TipoPresentismos\Controllers\CrudController::class . '@delete')->name('configuracion.licencia.delete');
+
+        Route::get('fecha/cierre', \Cat\Modules\Configuracion\FechaCierrePeriodo\Controllers\CrudController::class . '@index')
+            ->name('configuracion.fecha.cierre.index');
+
+        Route::post('fecha/cierre', \Cat\Modules\Configuracion\FechaCierrePeriodo\Controllers\CrudController::class . '@update')
+            ->name('configuracion.fecha.cierre.update');
+
+    });
--- app/Modules/Security/routes.php
+++ app/Modules/Security/routes.php
@@ -8,36 +8,27 @@
 | Rutas del presente modulo.
 |
 */
+
 use Illuminate\Support\Facades\Route;
 use Cat\Modules\Security\Controllers\PermissionCrudController;
 use Cat\Modules\Security\Controllers\UserCrudController;
 use Cat\Modules\Security\Controllers\RoleCrudController;
 
-Route::group(
-    ['middleware' => ['web']],
-    function (): void {
-        Route::group(
-            [
-                'prefix'     => 'seguridad',
-                'middleware' => [
-                    'web',
-//                    'admin',
-                ],
-            ],
-            function (): void {
-                Route::resource('permission', PermissionCrudController::class);
+Route::name('seguridad.')
+    ->prefix('seguridad')
+    ->middleware('web')
+    ->group(function (): void {
 
-                Route::resource('rol', RoleCrudController::class);
+        Route::resource('permission', PermissionCrudController::class);
+
+        Route::resource('rol', RoleCrudController::class);
 
 //                \CRUD::resource('role', RoleCrudController::class);
 
 //                Route::get('usuarios/search', UserCrudController::class . '@search');
 
-                Route::resource('usuario', UserCrudController::class);
+        Route::resource('usuario', UserCrudController::class);
 
 //                Route::get('usuario', UserCrudController::class . '@index')
 //                    ->name('listaUsuarios');
-            });
-        
-    }
-);
+    });
